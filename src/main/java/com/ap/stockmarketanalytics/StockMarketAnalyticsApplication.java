54e9feb98cd2d8d2a220e8d602b09efaaa4ebfac
Arun Patil <89706905+arunpatilgithub@users.noreply.github.com>
2023-06-12 12:37:33 -0600

    Add files via upload
    Skeleton commit

---
+++ src/main/java/com/ap/stockmarketanalytics/StockMarketAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.ap.stockmarketanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ap/stockmarketanalytics/StockMarketAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.stockmarketanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
